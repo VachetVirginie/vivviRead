010fc666a4d68a494f86f6043f5fb1dd34ab178f
VachetVirginie <vachetvirginie@gmail.com>
2025-11-21 08:49:06 +0100

    fix for build

--- src/composables/useSupabaseTest.ts
+++ src/composables/useSupabaseTest.ts
@@ -4,7 +4,7 @@ import { supabase } from '../lib/supabaseClient'
 export function useSupabaseTest() {
   onMounted(async () => {
 
-    const { data, error } = await supabase.from('books').select('*').limit(5)
+    const { error } = await supabase.from('books').select('*').limit(5)
 
     if (error) {
       console.error('[SupabaseTest] Erreur lors de la récupération des livres :', error)
